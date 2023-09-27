b372dc6e6d7576c025685a7e377c8430c89390bd
Parsa Merat <prs.merat@gmail.com>
2023-09-26 20:52:24 -0400

    file handler converts to int

--- file_handler.py
+++ file_handler.py
@@ -7,8 +7,21 @@ class FileHandlingTools(object):
         
     def load_transactions(self):
         #TODO: This fucntion reads the transactions from the csv file and returns a list of transactions
-        pass
+        transactions = []
+        with open(self.path_to_transactions_file, 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                row = list(dict.fromkeys(row))
+                row.sort()
+                row = [int(i) for i in row]
+                transactions.append(row)
+        return transactions
 
     def id_to_name(self, id):
         #TODO: This function returns the name of the item given its id which is read from the mapping file.
-            pass
+        with open(self.path_to_name_mapping_file, 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if row[0] == id:
+                    return row[1]
+        return None
